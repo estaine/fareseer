3ffc23b88c62e6ea04a2d01c21db884ca4d68bf0
Andrey Rykhalski <andrey.rykhalski@dmetrics.com>
2017-06-22 17:33:26 +0300

    Initial flyway script created

---
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,44 @@
+create table airline (
+    id bigint not null,
+    name varchar(255) not null,
+    iata_code char(2) not null,
+    primary key(id)
+);
+
+create table airport (
+    id bigint not null,
+    iata_code char(3) not null,
+    name varchar(255) not null,
+    alias varchar(255),
+    city varchar(255),
+    country varchar(255),
+    timezone varchar(255),
+    primary key(id)
+);
+
+create table currency (
+    id bigint not null,
+    iso_code char(3) not null,
+    name varchar(255) not null,
+    primary key(id)
+);
+
+create table flight (
+    id bigint not null,
+    airport_from_id bigint not null,
+    airport_to_id bigint not null,
+    airline_id bigint not null,
+    number int not null,
+    departure_utc_timestamp timestamp null,
+    arrival_utc_timestamp timestamp null,
+    fare double not null,
+    currency_id bigint not null,
+    primary key(id),
+    foreign key(airport_from_id) references airport(id),
+    foreign key(airport_to_id) references airport(id),
+    foreign key(airline_id) references airline(id),
+    foreign key(currency_id) references currency(id)
+);
+
+
+
